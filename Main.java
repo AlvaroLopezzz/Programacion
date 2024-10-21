b628c72639f83729b9e6afd02d190648a662bac3
AlvaroLopezzz <lopezmartineza185@gmail.com>
2024-10-21 12:29:40 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,73 +1,86 @@
-package SimpsonProgF;
+package com.example.main;
 
-import java.sql.SQLOutput;
 
-public class SimpsonProgF {
+import java.util.Random;
+
+public class Main{
     private static final int MAX_FILA_TABLERO = 10;
     private static final int MAX_COLUMNA_TABLERO = 10;
 
     private static char[][] tablero;
 
-    public static void main(String[] args) {
-
-        tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
-
+    private static void imprimirTablero() {
         for (int i = 0; i < MAX_FILA_TABLERO; i++) {
             for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
 
-                tablero[i][j] = 'L';
+                System.out.println(tablero[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+    private static void asignarLibreACasillasLibres(char caracter){
+        for (int i = 0; i < MAX_FILA_TABLERO; i++) {
+            for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
+
+                tablero[i][j] = caracter;
             }
         }
-        imprimirTablero();
-
-
-        Random aleatorio = new Ramdom();
-        int filaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
-        int columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-
-        tablero[filaAleatorio][columnaAleatorio] = 'B';
-
-
-        imprimirTablero();
-
-        int filaAleatorioHomer;
-        int columnaAleatorioHomer;
-
-        for (int i = 0; i < 10; i++) {
+    }
+    private static void asignarPersonajesACasillasLibre(char caracter, int numRepeticiones) {
+        Random aleatorio = new Random();
+        int filaAleatorio;
+        int columnaAleatorio;
+        for (int i = 0; i < numRepeticiones; i++) {
             do {
-                filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO);
-                columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-            } while (tablero[filaAleatorioHomer][columnaAleatorioHomer] != 'L');
-
-
-            tablero[filaAleatorioHomer][columnaAleatorioHomer] = 'H';
+                filaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
+                columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+            } while (tablero[filaAleatorio][columnaAleatorio] != 'L');
+            tablero[filaAleatorio][columnaAleatorio] = caracter;
         }
+    }
+    //  private static void asignarCaracterACasillasLibres ( char caracter) {
+    //     Random aleatorio = new Random();
+    //   int filaAleatorioHomer;
+    // int columnaAleatorioHomer;
+
+    //for (int i = 0; i < 10; i++) {
+    //  do {
+    //   filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO);
+    //   columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+    //} while (tablero[filaAleatorioHomer][columnaAleatorioHomer] != 'L');
+
+
+    // tablero[filaAleatorioHomer][columnaAleatorioHomer] = caracter;
+    //}
+//        }
+    public static void main (String [] args){
+
+        tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
+
+        asignarLibreACasillasLibres('L');
         imprimirTablero();
-        int filaAleatorioMuro;
-        int columnaAleatorioMuro;
-
-        for (int i = 0; i < 10; i++) {
-            do {
-                filaAleatorioMuro = aleatorio.nextInt(MAX_FILA_TABLERO);
-                columnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-            } while (tablero[filaAleatorioMuro][columnaAleatorioMuro] != 'L');
 
 
-            tablero[filaAleatorioMuro][columnaAleatorioMuro] = 'M';
-        }
+        asignarPersonajesACasillasLibre('B', 1);
+
+
+        imprimirTablero();
+
+        asignarPersonajesACasillasLibre('H', 10);
+
+        imprimirTablero();
+        asignarPersonajesACasillasLibre('M', 10);
+        imprimirTablero();
+        tablero[MAX_FILA_TABLERO - 1][MAX_COLUMNA_TABLERO - 1] = 'F';
         imprimirTablero();
     }
-        private static void imprimirTablero() {
-            for (int i = 0; i < MAX_FILA_TABLERO; i++) {
-                for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
 
-                    System.out.println(tablero[i][j] + " ");
-                }
-                System.out.println("");
-            }
 
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-        }
+
 }
